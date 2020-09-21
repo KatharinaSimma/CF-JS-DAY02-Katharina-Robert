2a1a7628fd9eda85350e75894ff92678f53942cc
Katharina Simma <katharina.simma@diebin.at>
2020-09-21 10:22:31 +0200

    minor changes

---
+++ basic excercises.js
@@ -0,0 +1,3 @@
+// Basic Exercise
+/* Create a program that according to the temperature will provide you with the information, whether the weather is cold or moderate. This program should generate a different random number every time, that should be between -5c and 25c. If the temperature is between -5c and 10c print "The weather is cold" otherwise print "The weather is moderate".*/
+
